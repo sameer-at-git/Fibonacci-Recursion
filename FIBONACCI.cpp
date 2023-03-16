fd13d91053627aa8e1cde7f5047cd4dfa5285cc8
Sameer Sayed <100744720+sameer-at-git@users.noreply.github.com>
2023-03-16 19:15:26 +0600

    Add files via upload

---
+++ FIBONACCI.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fibonacci(int n){
+if(n<=1){
+        return n;
+    }
+    else{return fibonacci(n-1)+fibonacci(n-2);}
+}
+
+int main(){
+    int n;
+    cout<<"Enter a value:"<<endl;
+    cin>>n;
+    cout<<fibonacci(n);
+return 0;
+}
